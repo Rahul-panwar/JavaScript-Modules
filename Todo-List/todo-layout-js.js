cd53de87662c9a50830c5be3ae4087bddb574b6e
Rahul-panwar <rahulpanwar1205@gmail.com>
2019-11-19 21:18:32 +0530

    Custom JavaScript modules added

---
+++ Todo-List/todo-layout-js.js
@@ -0,0 +1,11 @@
+function addTask(buttonParam){
+    buttonId = buttonParam.id;
+    taskList = document.getElementById('taskList');
+    task = document.getElementById('enterTask').value;
+
+    taskItem = document.createElement('div');
+    taskItem.setAttribute('id', '');
+    
+
+    console.log(task);
+}
